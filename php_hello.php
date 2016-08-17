75a8ee32b31119136d57a4c7d46035f6fcc73de4
James Dang <tld001@ucsd.edu>
2016-08-16 18:20:34 -0700

    cgi setup

--- php_hello.php
+++ php_hello.php
@@ -18,17 +18,16 @@
     <ul>
         <li><a href="index.html">Home</a></li>
         <li><a href="README.html">README</a></li>
+        
         <li><a href="php_hello.php">PHP Hello</a></li>
         <li><a href="php_environment.php">PHP Environment</a></li>
         <li><a href="php_form.php">PHP Form</a></li>
-        <li><a href="php_session1.php">PHP Session1</a></li>
-        <li><a href="php_session2.php">PHP Session2</a></li>
+        <li><a href="php_session.php">PHP Session1</a></li>
 
-        <li><a href="cgi_hello.html">CGI Hello</a></li>
-        <li><a href="cgi_environment.html">CGI Environment</a></li>
-        <li><a href="cgi_form.html">CGI Form</a></li>
-        <li><a href="cgi_session1.html">CGI Session1</a></li>
-        <li><a href="cgi_session2.html">CGI Session2</a></li>
+        <li><a href="cgi-bin/hello.cgi">CGI Hello</a></li>
+        <li><a href="cgi-bin/environment.cgi">CGI Environment</a></li>
+        <li><a href="cgi-bin/form.cgi">CGI Form</a></li>
+        <li><a href="cgi-bin/session.cgi">CGI Session1</a></li>
     <ul>
   <nav>
   <section>
