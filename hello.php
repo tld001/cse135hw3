bafb0dd891cc0795e765bd7ea32f6f164f41b4f7
James Dang <tld001@ucsd.edu>
2016-08-16 23:24:19 -0700

    php session 2

--- hello.php
+++ hello.php
@@ -35,7 +35,7 @@
         <li><a href="php_hello.php">PHP Hello</a></li>
         <li><a href="php_environment.php">PHP Environment</a></li>
         <li><a href="php_form.php">PHP Form</a></li>
-        <li><a href="php_session.php">PHP Session</a></li>
+        <li><a href="sessionpage1.php">PHP Session</a></li>
 
         <li><a href="cgi-bin/hello.cgi">CGI Hello</a></li>
         <li><a href="cgi-bin/environment.cgi">CGI Environment</a></li>
