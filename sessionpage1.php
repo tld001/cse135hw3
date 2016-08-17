f349513fc27bfe1a75cd8bdfa5a4f3c4377f8cdb
James Dang <tld001@ucsd.edu>
2016-08-16 23:22:08 -0700

    php session 1

---
+++ sessionpage1.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>James Dang</title>
+  <link href="http://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet" type="text/css" >
+  <link rel="stylesheet" type="text/css" href=style.css>
+</head>
+
+<body>
+  <nav>
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="README.html">README</a></li>
+        
+        <li><a href="php_hello.php">PHP Hello</a></li>
+        <li><a href="php_environment.php">PHP Environment</a></li>
+        <li><a href="php_form.php">PHP Form</a></li>
+        <li><a href="sessionpage1.php">PHP Session</a></li>
+
+        <li><a href="cgi-bin/hello.cgi">CGI Hello</a></li>
+        <li><a href="cgi-bin/environment.cgi">CGI Environment</a></li>
+        <li><a href="cgi-bin/form.cgi">CGI Form</a></li>
+        <li><a href="cgi-bin/session.cgi">CGI Session</a></li>
+    <ul>
+  <nav>
+  <section>
+        <br>
+        
+        <label>Please enter your name:
+          <input type="text" name="user_name" id="user_name">
+        </label>
+        <?php 
+          session_start();
+
+
+          $_SESSION['user_name']=$_POST['user_name'];
+
+          header( "Location: sessionpage2.php");
+          
+        ?>
+        <br>
+  <section>
+</body>
+</html>
