02597a6d53c4d0a57e2fbc9f48d2ec5d77429821
James Dang <tld001@ucsd.edu>
2016-08-31 16:20:49 -0700

    c3 8

--- action.php
+++ action.php
@@ -43,12 +43,12 @@
        */
 
        $temp = $_POST['movie_id'];
-       print $temp;
   	   $sql = "DELETE FROM movies WHERE movie_id='".$temp."' ";
 
        $result = mysqli_query($conn, $sql);
-
-		
+       if($result == false)
+       		print("hi");
+	
 	}
 
 	header('Location: index.php');
