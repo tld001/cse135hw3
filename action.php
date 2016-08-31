f5d3aaef13befd2a6e131a81d4647e928017b182
James Dang <tld001@ucsd.edu>
2016-08-31 14:41:34 -0700

    crud3 1

---
+++ action.php
@@ -0,0 +1,46 @@
+<?php
+	
+	require_once 'config.inc';
+	
+
+	$action = $_REQUEST['action'];
+	
+	if ($action == 'Add') {
+		
+       $title = $_REQUEST['title'];
+	   $studio = $_REQUEST['studio'];
+	   $year = $_REQUEST['year'];
+	   $boxoffice = $_REQUEST['boxoffice'];
+	   $poster = $_REQUEST['poster'];
+	   
+	   // SHOULD HAVE VALIDATION HERE!?
+		
+	
+	   $sql = "INSERT INTO users (title,studio,year,boxoffice) VALUES ('$title' , '$studio' , '$year' , '$boxoffice', '$poster')";
+	   $result = mysqli_query($conn, $sql);
+		
+		
+	} else if ($action == "Update") {
+		
+	   $title = $_REQUEST['title'];
+	   $studio = $_REQUEST['studio'];
+	   $year = $_REQUEST['year'];
+	   $boxoffice = $_REQUEST['boxoffice'];
+	   $movie_id = $_REQUEST['movie_id'];
+	   $poster = $_REQUEST['poster'];
+	
+	   $sql = "UPDATE users SET title='" .$title."' ,studio='".$studio."' ,year='".$year."' ,boxoffice='".$boxoffice."', poster='".$poster."' WHERE movie_id='".$movie_id."'";
+       $result = mysqli_query($conn, $sql);
+		
+	}  else if ($action == "Delete") {
+		
+  		
+       $sql = "DELETE FROM users WHERE movie_id='".$_POST['movie_id']."'"; 
+       $result = mysqli_query($conn, $sql);
+
+		
+	}
+
+	header('Location: index.php');
+	
+?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+	
+	// CONNECT TO DB
+    require_once 'config.inc';
+       
+    // FORM AND EXECUTE SOME QUERY
+    $sql = "SELECT title,studio,year,boxoffice, poster FROM movies ORDER BY boxoffice";
+    $result = mysqli_query($conn, $sql);
+    
+    $users = $result->fetch_all(MYSQLI_ASSOC);
+   
+	// RENDER THE VIEW
+    include('./views/app.tpl');
+?>
+
