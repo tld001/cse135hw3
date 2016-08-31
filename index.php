6eed43f9b50d9ac498d2db001132e90a7032af5f
James Dang <tld001@ucsd.edu>
2016-08-31 15:55:52 -0700

    c3 1

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+	
+	// CONNECT TO DB
+    require_once 'config.inc';
+       
+    // FORM AND EXECUTE SOME QUERY
+    $sql = "SELECT title,studio,year,boxoffice, poster FROM movies ORDER BY boxoffice DESC";
+    $result = mysqli_query($conn, $sql);
+    
+    $movies = $result->fetch_all(MYSQLI_ASSOC);
+   
+	// RENDER THE VIEW
+    include('./views/app.tpl');
+?>
+
